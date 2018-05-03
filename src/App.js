4514f3e28fee3acd12b73d609e7d4ee0467683c0
Alina Tudose <alina.tudose@travelbird.com>
2018-05-04 00:04:12 +0200

    read project from google sheets

--- src/App.js
+++ src/App.js
@@ -1,126 +1,20 @@
+
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import {Project} from './Project.js';
-import {projects} from './Projects.js';
+// import {projects} from './Projects.js';
 import {filters, filterLabels} from './filters.js';
 import classnames from 'classnames';
-// import GoogleLogin from 'react-google-login';
-
-// class ProjectForm extends React.Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             value: ''
-//         };
-//
-//         this.handleChange = this.handleChange.bind(this);
-//         this.handleSubmit = this.handleSubmit.bind(this);
-//     }
-//
-//     handleChange(event) {
-//         this.setState({value: event.target.value});
-//     }
-//
-//     handleSubmit(event) {
-//         console.log(this.props);
-//         event.preventDefault();
-//         const fields = ['one', 'two', 'three', 'four', 'five', 'six', 'seven'];
-//         const self = this
-//         let row = '';
-//         fields.map(field => {
-//             return row += '"'+ self.state.value +'",';
-//         })
-//         // Clean Row
-// 		row = row.slice(0, -1);
-//         console.log('row', row);
-//         // Config
-//         const gs_sid = '1bWOM_zbnNrSujOpiDU9rGdxwwSXGhppsU8mn_v9RhnI'; // Enter your Google Sheet ID here
-//         const gs_clid = '645401863724-r7o2jhsfotcq11jou0bsahbfun2c4t0t.apps.googleusercontent.com'; // Enter your API Client ID here
-//         const gs_clis = 'LrH2i1ksne0e9B4YK-o6W2Yd'; // Enter your API Client Secret here
-//         // const gs_rtok = this.props.accessToken; // Enter your OAuth Refresh Token here
-//         const gs_rtok = 'https://www.googleapis.com/oauth2/v4/token?code='+this.props.accessToken+'&client_id='+gs_clid+'&client_secret='+gs_clis+'&grant_type='
-//         let gs_atok = false;
-//         const gs_url = 'https://sheets.googleapis.com/v4/spreadsheets/'+gs_sid+'/values/A1:append?includeValuesInResponse=false&insertDataOption=INSERT_ROWS&responseDateTimeRenderOption=SERIAL_NUMBER&responseValueRenderOption=FORMATTED_VALUE&valueInputOption=USER_ENTERED';
-//         const gs_body = '{"majorDimension":"ROWS", "values":[['+row+']]}';
-//         console.log(gs_body);
-//          // HTTP Request Token Refresh
-//         let xhr = new XMLHttpRequest();
-//         xhr.open('POST', 'https://www.googleapis.com/oauth2/v4/token?client_id='+gs_clid+'&client_secret='+gs_clis+'&refresh_token='+gs_rtok+'&grant_type=refresh_token');
-//         xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-//         xhr.onload = function() {
-//             const response = JSON.parse(xhr.responseText);
-//             console.log('response', response);
-//             gs_atok = response.access_token;
-// 			// HTTP Request Append Data
-//             if ( gs_atok ) {
-//                 let xxhr = new XMLHttpRequest();
-//                 xxhr.open('POST', gs_url);
-//                 // xxhr.setRequestHeader('Content-length', gs_body.length);
-//                 xxhr.setRequestHeader('Content-type', 'application/json');
-//                 xxhr.setRequestHeader('Authorization', 'OAuth ' + gs_atok );
-//                 xxhr.onload = function() {
-// 					if(xxhr.status === 200) {
-// 						// Success
-//                         console.log('success');
-//                     }
-//                     else {
-// 						// Fail
-// 						console.log('failure');
-// 					}
-//                 };
-//                 xxhr.send(gs_body);
-//             }
-//         };
-//         xhr.send();
-//     }
-//
-//     render() {
-//         return (
-//             <form onSubmit={this.handleSubmit}>
-//                 <label>
-//                     One:
-//                     <input name="one" type="text" value={this.state.value.one} onChange={this.handleChange} />
-//                 </label>
-//                 <label>
-//                     Two:
-//                     <input name="two" type="text" value={this.state.value.two} onChange={this.handleChange} />
-//                 </label>
-//                 <label>
-//                     Three:
-//                     <input name="three" type="text" value={this.state.value.three} onChange={this.handleChange} />
-//                 </label>
-//                 <label>
-//                     Four:
-//                     <input name="four" type="text" value={this.state.value.four} onChange={this.handleChange} />
-//                 </label>
-//                 <label>
-//                     Five:
-//                     <input name="five" type="text" value={this.state.value.five} onChange={this.handleChange} />
-//                 </label>
-//                 <label>
-//                     Six:
-//                     <input name="six" type="text" value={this.state.value.six} onChange={this.handleChange} />
-//                 </label>
-//                 <label>
-//                     Seven:
-//                     <input name="seven" type="text" value={this.state.value.seven} onChange={this.handleChange} />
-//                 </label>
-//                 <input type="submit" value="Submit" />
-//             </form>
-//         );
-//     }
-// }
-
-//
-// const responseGoogle = (response) => {
-//   console.log(response);
-// }
+import { checkAuth, load } from './helpers/spreadsheet';
+// import { hash } from './helpers/utils';
+// import * as ls from './helpers/localStorage'
+import Alert from './Alert';
 
 class List extends React.Component {
     render () {
         const {projects, selectedFilters} = this.props
-        const searchResults = projects.filter((project) => project.details.toLowerCase().indexOf(this.props.filter.toLowerCase()) >= 0)
+        const searchResults = projects.filter((project) => project.details && project.details.toLowerCase().indexOf(this.props.filter.toLowerCase()) >= 0)
         const filteredProjects = selectedFilters.map(selectedFilter => searchResults.filter(project => project.tags.indexOf(selectedFilter) >= 0))
         const flatFilteredProjects = [].concat.apply([], filteredProjects)
         let unique_array = flatFilteredProjects.filter(function(elem, index, self) {
@@ -195,19 +89,53 @@ class ProjectFilterGroup extends React.Component {
     }
 }
 
-class App extends Component<> {
-    constructor() {
-        super();
+class App extends Component {
+    constructor(props) {
+        super(props);
         this.state = {
-            projects: projects,
+            projects: [],
             searchTerm: '',
             filters: [],
             accessToken: ''
         }
         this.updateSearch = this.updateSearch.bind(this)
         this.updateFilters = this.updateFilters.bind(this)
-        this.authenticate = this.authenticate.bind(this)
     }
+
+    componentDidMount() {
+        window.gapi.load('client', () => {
+            checkAuth(true, this.handleAuth.bind(this));
+        });
+    }
+
+    handleAuth(authResult) {
+        if (authResult && !authResult.error) {
+            this.setState({
+                authenticated: true
+            });
+            load(this.onLoad.bind(this))
+        } else {
+            this.setState({
+                authenticated: false
+            })
+        }
+    }
+
+    onLoad(data, error) {
+        console.log('dataaaa', data);
+        if (data) {
+            console.log('data exists');
+            this.setState({
+                ...data
+            });
+        }
+        else {
+            this.setState({
+                error: error
+            })
+        }
+    }
+
     updateSearch(inputValue) {
         this.setState({
             searchTerm: inputValue
@@ -226,14 +154,8 @@ class App extends Component<> {
             }))
         }
     }
-
-    authenticate(response) {
-        console.log(response);
-        response && response.accessToken && this.setState({accessToken: response.accessToken})
-    }
-
+    
     render() {
-
         return (
             <div className="App">
                 <div className="Header">
@@ -246,29 +168,49 @@ class App extends Component<> {
                 </div>
                 <div className="Content">
                     <div className="Navigation">
-                        {/*
-                        <GoogleLogin
-                            clientId="645401863724-r7o2jhsfotcq11jou0bsahbfun2c4t0t.apps.googleusercontent.com"
-                            accessType="code"
-                            buttonText="Login"
-                            onSuccess={this.authenticate}
-                            onFailure={this.authenticate}
-                          />
-                          */}
                         <div className="Navigation__filters">
                             { Object.keys(filters).map(filter => {
                                 return <ProjectFilterGroup key={filter} filter={filter} updateFilters={this.updateFilters} />
                             })}
                         </div>
                     </div>
-                    <div className="Projects">
-
-                        {/*<ProjectForm accessToken={this.state.accessToken}/>*/}
-                        <List filter={this.state.searchTerm} selectedFilters={this.state.filters} projects={this.state.projects}/>
-                    </div>
+                    { this.renderContent() }
                 </div>
             </div>
         );
+    }
+
+    renderContent() {
+        console.log('this state', this.state.authenticated);
+        const projects = this.state.projects;
+        if (this.state.authenticated !== true) {
+            return (
+                <button onClick={ this.authenticate.bind(this) } className="btn">Connect with Google</button>
+            );
+        }
+        else if (projects.length) {
+            return (
+                <div className="Projects">
+                    {/*<ProjectForm accessToken={this.state.accessToken}/>*/}
+                    <List filter={this.state.searchTerm} selectedFilters={this.state.filters} projects={this.state.projects}/>
+                </div>
+            );
+        }
+        else if (this.state.error) {
+            return (
+                <Alert error={ this.state.error } />
+            );
+        }
+        else {
+            return (
+                <div className="loader" />
+            );
+        }
+    }
+
+    authenticate(e) {
+        e.preventDefault();
+        checkAuth(false, this.handleAuth.bind(this));
     }
 }
 
---
+++ src/helpers/spreadsheet.js
@@ -0,0 +1,70 @@
+// import { orderBy } from 'lodash';
+// import { hash } from './utils';
+// import { get } from './localStorage';
+
+import config from '../config';
+
+/**
+* Get the user authentication status
+*/
+export function checkAuth(immediate, callback) {
+    window.gapi.auth.authorize({
+        'client_id': config.clientId,
+        'scope': config.scope,
+        'immediate': immediate
+    }, callback);
+}
+
+/**
+* Load the projects from the spreadsheet
+*/
+export function load(callback) {
+    window.gapi.client.load('sheets', 'v4', () => {
+        window.gapi.client.sheets.spreadsheets.values.get({
+            spreadsheetId: config.spreadsheetId,
+            range: 'A1:I'
+        }).then((response) => {
+            console.log(response);
+            const data = response.result.values
+            console.log(data);
+
+            const projectFields = data[0]
+            console.log(projectFields);
+            let projects = data.map((project, i) => {
+                let row = i + 2 // Save row ID fore later update
+                let id = i
+                let title = project[0]
+                let details = project[8]
+                let tags = [
+                    project[1], project[2], project[3], project[4], project[5], project[6], project[7]
+                ]
+
+                return {
+                    row,
+                    id,
+                    title,
+                    details,
+                    tags
+                }
+            })
+
+            callback({
+                projects,
+            });
+        }, (response) => {
+            callback(false, response.result.error);
+        });
+    });
+}
+
+/**
+* Update a single cell value
+*/
+export function updateCell(column, row, value, successCallback, errorCallback) {
+    window.gapi.client.sheets.spreadsheets.values.update({
+        spreadsheetId: config.spreadsheetId,
+        range: 'Sheet1!' + column + row,
+        valueInputOption: 'USER_ENTERED',
+        values: [ [value] ]
+    }).then(successCallback, errorCallback);
+}
