7eacd8a22c6b556180bef1e2d336983de6d5dd56
Alina Tudose <alina.tudose@travelbird.com>
2018-05-07 20:04:35 +0200

    remove config

--- src/config.js
+++
@@ -1,5 +0,0 @@
-export default {
-  clientId: "645401863724-loav0ft01scs8allb3m8j25hh0nn6nbp.apps.googleusercontent.com",
-  scope: 'https://www.googleapis.com/auth/spreadsheets',
-  spreadsheetId: '1bWOM_zbnNrSujOpiDU9rGdxwwSXGhppsU8mn_v9RhnI'
-}
